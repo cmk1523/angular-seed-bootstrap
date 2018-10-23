e6e599a3f2b91cbd1c49e7741294d05aeedc222e
Christopher Kolodziejczyk <cmk1523@gmail.com>
2018-10-22 23:27:20 -0400

    - update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FooterComponent} from './footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
       ],
+      imports: [
+      HttpClientModule,
+      RouterTestingModule.withRoutes([])
+    ]
     }).compileComponents();
   }));
 
@@ -14,18 +24,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'yyy'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('yyy');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to yyy!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {AboutComponent} from './about/about.component';
+import {HeaderComponent} from './header/header.component';
+import {SettingsComponent} from './settings/settings.component';
+import {SupportComponent} from './support/support.component';
+import {SignoutComponent} from './signout/signout.component';
+import {FooterComponent} from './footer/footer.component';
+import {LegalComponent} from './legal/legal.component';
+import {AppRouting} from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    SettingsComponent,
+    SupportComponent,
+    SignoutComponent,
+    FooterComponent,
+    LegalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
